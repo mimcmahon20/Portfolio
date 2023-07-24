7ccaa1d63f94fd1411fa822dc783a897f1c24aba
mimcmahon20 <mimcmahon20@vt.edu>
2023-07-23 20:43:42 -0400

    Attempting to fix dev depend.

--- src/components/ProjectMedia.jsx
+++ src/components/ProjectMedia.jsx
@@ -4,10 +4,11 @@ import "../styles/ProjectMedia.css";
 export default function ProjectMedia(props) {
   const { url, type } = props;
 
+
   return (
     <div className="project-media">
       <video className="project-video" autoPlay muted controls loop>
-        <source src={url} type="video/mp4" />
+        <source src={url} type="video/mp4" className={type} />
       </video>
     </div>
   );
