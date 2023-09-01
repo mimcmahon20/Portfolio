ead00bb938a229aff08d8a3e9413546149a64d9c
Maguire McMahon <mimcmahon20@vt.edu>
2023-08-31 22:42:28 -0400

    Bug fix for logo
    -Slowed down logo animation slightly

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,6 +7,7 @@ export default function Logo() {
       <lottie-player
         autoplay
         mode="normal"
+        speed="0.9"
         src="https://lottie.host/37884961-3491-4f6c-8b5e-70e87f3430db/XmMFvM4Ljp.json"
       ></lottie-player>
     </div>
