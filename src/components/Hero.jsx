70d52d552dce096e7552c5b6cfa3cd618fccb9c8
Maguire McMahon <mimcmahon20@vt.edu>
2023-08-31 21:50:28 -0400

    scroller & hero section change
    removed arrows added a scroller animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useEffect } from "react";
 import Nav from "./Nav";
 import HeroText from "./HeroText";
-import HeroArrow from "./HeroArrow";
+
 import "../styles/Hero.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { TextPlugin } from "gsap/TextPlugin";
+import Scroller from "./Scroller";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
@@ -15,7 +16,8 @@ export default function Hero() {
 
   //animating the arrows 
   useEffect(() => {
-    const arrows = HeroRef.current.querySelectorAll(".arrow");
+
+    const scroller = HeroRef.current.querySelector(".scroller");
     const firstLine = HeroRef.current.querySelector(".first-line");
     const secondLine = HeroRef.current.querySelector(".second-line");
 
@@ -23,16 +25,16 @@ export default function Hero() {
 
     if(window.scrollY < 100) {
       //animating lines of text
-      HeroTimeline.fromTo(firstLine, { scale: 0 }, { scale: 1, duration: 1, delay: 0.5});
-      HeroTimeline.fromTo(secondLine, { scale: 0 }, { scale: 1, duration: 1, delay: 0.5}, "<");
+      HeroTimeline.fromTo(firstLine, { opacity: 0, x: window.innerWidth/2 }, { opacity: 1, x:0, duration: .4, delay: 0.6});
+      HeroTimeline.fromTo(secondLine, { opacity: 0, x: -window.innerWidth/2 }, { opacity: 1, x: 0, duration: .4, delay: 0.2}, "<");
       
       //animating arrows
-      HeroTimeline.fromTo(arrows, { scale: 0, y: -50}, { delay: 0.5, scale: 1, y: 0, stagger: 0.1, duration: .5});
+      HeroTimeline.fromTo(scroller, { opacity: 0}, { delay: 0.4, opacity: 1, duration: .2});
   
       //animating small scroll to projects in bottom right
       HeroTimeline.fromTo(".scroll-to-projects", { opacity: 0 }, { opacity: 1, duration: 1, delay: 1.5 }, "-=2");
 
-      HeroTimeline.fromTo(arrows, {y: 0}, { y: -500, duration: 1, stagger: .1, scrollTrigger: {
+      HeroTimeline.fromTo(scroller, {y: 0}, { y: 250, duration: 1, stagger: .1, scrollTrigger: {
         trigger: ".hero",
         start: "50% 50%",
         end: "bottom 50%",
@@ -78,26 +80,23 @@ export default function Hero() {
     });
   });
 
-  function calcArrows(height) {
-    let arrows = [];
-    for (let i = 0; i < 8; i++) {
-      arrows.push(<HeroArrow key={i} width={height - i * 100 + 50} />);
-    }
-    return arrows;
-  }
+  // function calcArrows(height) {
+  //   let arrows = [];
+  //   for (let i = 0; i < 8; i++) {
+  //     arrows.push(<HeroArrow key={i} width={height - i * 100 + 50} />);
+  //   }
+  //   return arrows;
+  // }
 
-  let height = window.innerHeight / 2;
 
-  window.addEventListener("resize", () => {
-    height = window.innerHeight / 2;
-  });
 
   return (
     <div ref={HeroRef} className="hero">
       <Nav />
       <HeroText />
       <div className="arrows-container">
-        {calcArrows(height)}
+        {/* {calcArrows(height)} */}
+        <Scroller></Scroller>
         <h3 className="scroll-to-projects">Scroll for projects</h3>
       </div>
     </div>
