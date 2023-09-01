93553d1509b0323f5a732a48377691ee9588b9f2
mimcmahon20 <mimcmahon20@vt.edu>
2023-09-01 17:26:36 -0400

    footer & mail animation
    Figure out how to make animation only play on hover?

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "../styles/Footer.css";
+import ContactAnimation from "./ContactAnimation";
 
 export default function Footer() {
     return (
         <footer className="footer">
-            <h3 className="footer-header">Let's make something happen!</h3>
+            <h3 className="footer-header">Let's make something <span className="footer-span">Amazing</span>!</h3>
             <div className="footer-button-container">
-                <button className="footer-button">Contact</button>
+                <a className="footer-button" href="mailto:mimcmahon20@vt.edu?&subject=Let's get in touch&body=Dear Maguire McMahon" target="_blank" rel="noreferrer">Contact <ContactAnimation></ContactAnimation></a>
             </div>
         </footer>
     )
